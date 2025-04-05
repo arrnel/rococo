33e217d47ec27dccc867e2faf471da1ec0d1a669
Arrnel <arrnel@vk.com>
2025-04-05 17:15:10 +0300

    Added FAKE DB tests

---
+++ rococo-tests/src/test/java/org/rococo/tests/tests/fake/db/CountryDbTest.java
@@ -0,0 +1,101 @@
+package org.rococo.tests.tests.fake.db;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
+import org.rococo.tests.enums.CountryCode;
+import org.rococo.tests.enums.ServiceType;
+import org.rococo.tests.jupiter.annotation.Country;
+import org.rococo.tests.jupiter.annotation.meta.DbTest;
+import org.rococo.tests.jupiter.annotation.meta.InjectService;
+import org.rococo.tests.model.CountryDTO;
+import org.rococo.tests.service.CountryService;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Isolated
+@DbTest
+@Feature("FAKE")
+@Story("[DB] Paintings tests")
+@DisplayName("[DB] Paintings tests")
+@ParametersAreNonnullByDefault
+class CountryDbTest {
+
+    @InjectService(ServiceType.DB)
+    private CountryService countryService;
+
+    @Country
+    @Test
+    @DisplayName("Can get country by id")
+    void canGetCountryByIdTest(CountryDTO country) {
+
+        // Steps
+        var result = countryService.findById(country.getId()).orElse(new CountryDTO());
+
+        // Assertions
+        assertAll(
+                () -> assertEquals(country.getName(), result.getName()),
+                () -> assertEquals(country.getCode(), result.getCode())
+        );
+
+    }
+
+    @Test
+    @DisplayName("Returns Optional.empty() if search country by unknown id")
+    void canGetEmptyCountryByUnknownIdTest() {
+
+        // Steps
+        var result = countryService.findById(UUID.randomUUID());
+
+        // Assertions
+        assertTrue(result.isEmpty());
+
+    }
+
+    @Country
+    @Test
+    @DisplayName("Can get country by code")
+    void canGetCountryByCodeTest(CountryDTO country) {
+
+        // Steps
+        var result = countryService.findByCode(country.getCode()).orElse(new CountryDTO());
+
+        // Assertions
+        assertAll(
+                () -> assertEquals(country.getId(), result.getId()),
+                () -> assertEquals(country.getName(), result.getName())
+        );
+
+    }
+
+    @Test
+    @DisplayName("Returns Optional.empty() if search country by unknown country code")
+    void canGetEmptyCountryByUnknownCountryCodeTest() {
+
+        // Steps
+        var result = countryService.findByCode(CountryCode.EMPTY);
+
+        // Assertions
+        assertTrue(result.isEmpty());
+
+    }
+
+    @Test
+    @DisplayName("Can get all countries")
+    void canGetAllCountriesTest() {
+
+        // Steps
+        var result = countryService.findAll();
+
+        // Assertions
+        assertEquals(251, result.size());
+
+    }
+
+}
