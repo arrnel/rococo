44fc563625f7214d40731ce45e30dc5b94ced8b8
Arrnel <arrnel@vk.com>
2025-04-12 11:09:06 +0300

    Adapted rococo-museums for microservices interaction

--- rococo-museums/src/main/java/org/rococo/museum/ex/MuseumAlreadyExistException.java
+++
@@ -1,9 +0,0 @@
-package org.rococo.museum.ex;
-
-public class MuseumAlreadyExistException extends RuntimeException {
-
-    public MuseumAlreadyExistException(String museumName) {
-        super("Museum with name = [%s] already exists".formatted(museumName));
-    }
-
-}
---
+++ rococo-museums/src/main/java/org/rococo/museum/ex/advice/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package org.rococo.museum.ex.advice;
+
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.advice.GrpcAdvice;
+import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
+
+@Slf4j
+@GrpcAdvice
+public class GlobalExceptionHandler {
+
+    @GrpcExceptionHandler(Exception.class)
+    public StatusRuntimeException handleException(Exception ex) {
+        log.error(ex.getMessage());
+        return Status.UNKNOWN
+                .withDescription(ex.getMessage())
+                .withCause(ex)
+                .asRuntimeException();
+    }
+
+}
