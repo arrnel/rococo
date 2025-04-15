64e2b5bdcd15a279bc0841cd3540372786b01cac
Arrnel <arrnel@vk.com>
2025-04-15 23:55:53 +0300

    Added ordering for MuseumExceptionHandler

--- rococo-museums/src/main/java/org/rococo/museum/ex/advice/MuseumExceptionHandler.java
+++ rococo-museums/src/main/java/org/rococo/museum/ex/advice/MuseumExceptionHandler.java
@@ -5,8 +5,11 @@ import io.grpc.StatusRuntimeException;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 import org.rococo.museum.ex.*;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 @GrpcAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MuseumExceptionHandler {
 
     @GrpcExceptionHandler({MuseumAlreadyExistsException.class, ImageAlreadyExistsException.class})
