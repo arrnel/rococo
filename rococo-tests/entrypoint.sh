14f0f66291d1f1c61580412fe6ff9628ed899c02
Arrnel <arrnel@vk.com>
2026-03-08 20:48:34 +0300

    Changed test filtering logic

--- rococo-tests/entrypoint.sh
+++ rococo-tests/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-TAG_OPT=""
-if [ -n "${TAG_EXPR//[[:space:]]/}" ]; then
-    TAG_OPT="--include-tags '$TAG_EXPR'"
-fi
-
 echo "Running gradlew with TAG_EXPR: \"$TAG_EXPR\" and TEST_ARGS: \"$TEST_ARGS\""
 
-exec ./gradlew test -Dtest.env=docker -Dtests.db_cleanup=false -Duser.timezone=Europe/Moscow $TAG_OPT $TEST_ARGS
+exec ./gradlew test -Dtest.env=docker -Dtests.db_cleanup=false -Duser.timezone=Europe/Moscow $TEST_ARGS
